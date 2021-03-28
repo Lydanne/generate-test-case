0892626f96a31d8613556b97af03bec08518ca03
WumaCoder <1829913225@qq.com>
2021-03-28 10:53:53 +0800

    fix: update API

--- src/generate/string.js
+++ src/generate/string.js
@@ -79,32 +79,23 @@ function randomChar(strList = []) {
 /**
  *
  * @param {number} len 随机生成长度为len的字符串
+ * @param {string[]} strList 待选字符数组，如果不传入表示随机生产一个字母
  */
-function randomStr(len) {
-  return repeat(() => randomChar(), len).join("");
+function randomStr(len, strList = []) {
+  return repeat(() => randomChar(strList), len).join("");
 }
 
 /**
  *
  * @param {number} n 生成由n个单词组成的字符串，没有.号。
+ * @param {string[]} strList 待选字符数组，如果不传入表示随机生产一个字母
  */
-function randomWord(n) {
-  return repeat(() => randomStr(random(3, 7)), n).join(" ");
-}
-
-/**
- *
- * @param {string[]} strList 待选择的字符串数组
- * @param {number} n 产生多少个
- * @param {string} separator 分割符
- */
-function repeatRandomChar(strList, n, separator = "") {
-  return repeat(randomChar.bind(null, strList), n).join(separator);
+function randomWord(n, strList = []) {
+  return repeat(() => randomStr(random(3, 7), strList), n).join(" ");
 }
 
 module.exports = {
   randomChar,
-  repeatRandomChar,
   randomStr,
   randomWord,
 };
