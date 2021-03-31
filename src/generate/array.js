c9d54b4f84d8c08b84398b5b42b2fdd84e5ca1f2
WumaCoder <1829913225@qq.com>
2021-03-31 14:37:37 +0800

    fix: array generate return type

--- src/generate/array.js
+++ src/generate/array.js
@@ -7,20 +7,18 @@ const { randomStr } = require("./string");
  * @param {number} start 开始数
  * @param {number} end 结束的数
  * @param {number} len 数组个数
- * @param {string} separator 分割符
  */
-function randomNumArr(start, end, len, separator = ",") {
-  return repeat(() => random(start, end), len).join(separator);
+function randomNumArr(start, end, len) {
+  return repeat(() => random(start, end), len);
 }
 
 /**
  * 生成长度为strLen的字符串数组
  * @param {number} strLen 字符串长度
  * @param {number} len 数组个数
- * @param {string} separator 分割符号
  */
-function randomStrArr(strLen, len, separator = ",") {
-  return repeat(() => randomStr(strLen), len).join(separator);
+function randomStrArr(strLen, len) {
+  return repeat(() => randomStr(strLen), len);
 }
 
 /**
@@ -28,15 +26,14 @@ function randomStrArr(strLen, len, separator = ",") {
  * @param {number} start 初始值
  * @param {number} len 长度
  * @param {number} step 步长
- * @param {string} separator 分割符号
  */
-function incrNumArr(start, len, step = 1, separator = ",") {
+function incrNumArr(start, len, step = 1) {
   const result = [];
   for (let i = 0; i < len; i++) {
     result.push(start);
     start += step;
   }
-  return result.join(separator);
+  return result;
 }
 
 module.exports = {
